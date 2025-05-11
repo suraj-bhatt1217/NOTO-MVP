5c3dc9f82c52b5e99211cb7fb48e0120d8ec086b
Suraj Bhatt <surajbhatt54321@gmail.com>
2025-05-11 11:20:35 +0530

    revert replace

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ firebase_credentials = {
     "type": "service_account",
     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": (os.getenv("FIREBASE_PRIVATE_KEY")),
+    "private_key": (os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n")),
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
